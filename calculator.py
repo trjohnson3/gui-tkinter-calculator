dca71249206a817eb5ac9be9caa12c0d692681ea
Tom Johnson <trjohnson3@gmail.com>
2024-06-18 08:56:39 -0500

    initial setup

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#calculator
+import tkinter
+
+#Define window
+root = tkinter.Tk()
+root.title('Calculator')
+root.iconbitmap('./images/calculator.ico')
+root.geometry('300x400')
+root.resizable(0, 0)
+
+#Define fonts and colors
+dark_green = '#93af22'
+light_green = '#acc253'
+white_green = '#edefe0'
+button_font = ('Arial', 18)
+display_font = ('Arial', 30)
+
+#Define funcitons
+
+#Define layout
+
+#Run app
+root.mainloop()
